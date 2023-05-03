9110e9f1306b65a78e602008ae6863d692162598
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-05-03 12:42:39 -0700

    update Homecontroller

--- src/main/java/com/chamcocat/songr/HomeController.java
+++ src/main/java/com/chamcocat/songr/HomeController.java
@@ -1,54 +1,73 @@
 package com.chamcocat.songr;
 
 import com.chamcocat.songr.model.Album;
+import com.chamcocat.songr.model.Song;
 import com.chamcocat.songr.repository.AlbumRepository;
+import com.chamcocat.songr.repository.SongRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
-    private final AlbumRepository albumRepository;
+    @Autowired
+    private AlbumRepository albumRepository;
 
-    public HomeController(AlbumRepository albumRepository) {
-        this.albumRepository = albumRepository;
-    }
+    @Autowired
+    private SongRepository songRepository;
 
     @GetMapping("/")
-    public String index() {
+    public String home(Model model) {
         return "index";
     }
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "hello-world";
     }
 
     @GetMapping("/capitalize/{text}")
-    public String capitalize(@PathVariable("text") String text, Model model) {
-        model.addAttribute("capitalized", text.toUpperCase());
+    public String capitalize(@PathVariable String text, Model model) {
+        model.addAttribute("text", text.toUpperCase());
         return "capitalize";
     }
 
     @GetMapping("/albums")
     public String albums(Model model) {
-        model.addAttribute("albums", albumRepository.findAll());
+        List<Album> albums = albumRepository.findAll();
+        model.addAttribute("albums", albums);
         return "albums";
     }
 
     @PostMapping("/albums")
-    public String addAlbum(@RequestParam String title,
-                           @RequestParam String artist,
-                           @RequestParam Integer song_count,
-                           @RequestParam Integer length_in_seconds,
-                           @RequestParam String image_url) {
-        Album album = new Album(title, artist, song_count, length_in_seconds, image_url);
+    public String addAlbum(@ModelAttribute Album album) {
         albumRepository.save(album);
         return "redirect:/albums";
+    }
+
+    @GetMapping("/albums/{id}")
+    public String albumDetails(@PathVariable Long id, Model model) {
+        Album album = albumRepository.findById(id).orElse(null);
+        model.addAttribute("album", album);
+        return "album-details";
+    }
+
+    @PostMapping("/albums/{id}/songs")
+    public String addSongToAlbum(@PathVariable Long id, @ModelAttribute Song song) {
+        Album album = albumRepository.findById(id).orElse(null);
+        if (album != null) {
+            song.setAlbum(album);
+            songRepository.save(song);
+        }
+        return "redirect:/albums/" + id;
     }
 }
