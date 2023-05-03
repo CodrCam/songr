7ccea315275ee288328906a23f2ceaec2232aed1
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-05-02 22:28:22 -0700

    add repo funtionality

--- src/main/java/com/chamcocat/songr/HomeController.java
+++ src/main/java/com/chamcocat/songr/HomeController.java
@@ -1,15 +1,28 @@
 package com.chamcocat.songr;
 
+import com.chamcocat.songr.model.Album;
+import com.chamcocat.songr.repository.AlbumRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.Arrays;
-import java.util.List;
 
-@SuppressWarnings("unused")
+
 @Controller
 public class HomeController {
+
+    private final AlbumRepository albumRepository;
+
+    public HomeController(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
     @GetMapping("/hello")
     public String hello() {
@@ -18,31 +31,24 @@ public class HomeController {
 
     @GetMapping("/capitalize/{text}")
     public String capitalize(@PathVariable("text") String text, Model model) {
-        String capitalizedText = text.toUpperCase();
-        model.addAttribute("capitalizedText", capitalizedText);
+        model.addAttribute("capitalized", text.toUpperCase());
         return "capitalize";
-    }
-
-    @GetMapping("/")
-    public String splash() {
-        return "index";
     }
 
     @GetMapping("/albums")
     public String albums(Model model) {
-        Album album1 = new Album("DAMN.", "Kendrick Lamar", 14, 3600, "https://static.hiphopdx.com/2017/04/Kendrick-Lamar-DAMN-album-cover-featured-827x620.jpg");
-        Album album2 = new Album("Traumazine", "Megan Thee Stallion", 12, 4200, "https://imageUrl2.com");
-        Album album3 = new Album("I Decided.", "Big Sean", 14, 3200, "https://imageUrl3.com");
-
-        List<Album> albums = Arrays.asList(album1, album2, album3);
-        model.addAttribute("albums", albums);
+        model.addAttribute("albums", albumRepository.findAll());
         return "albums";
     }
 
-    @GetMapping("/error")
-    public String handleError() {
-        return "error";
+    @PostMapping("/albums")
+    public String addAlbum(@RequestParam String title,
+                           @RequestParam String artist,
+                           @RequestParam Integer song_count,
+                           @RequestParam Integer length_in_seconds,
+                           @RequestParam String image_url) {
+        Album album = new Album(title, artist, song_count, length_in_seconds, image_url);
+        albumRepository.save(album);
+        return "redirect:/albums";
     }
-
 }
-
