0d634d26d0d5cf88f116129ac6d0a2a80d1b7b47
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-05-02 22:28:59 -0700

    fix DB call

--- src/main/java/com/chamcocat/songr/model/Album.java
+++ src/main/java/com/chamcocat/songr/model/Album.java
@@ -1,10 +1,11 @@
 package com.chamcocat.songr.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "albums")
@@ -13,24 +14,22 @@ public class Album {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
     private String artist;
-    private int songCount;
-    private int length;
-    private String imageUrl;
+    private String image_url;
+    private int length_in_seconds;
+    private int song_count;
+    private String title;
 
     public Album() {
     }
 
-    public Album(String title, String artist, int songCount, int length, String imageUrl) {
-        this.title = title;
+    public Album(String artist, String image_url, int length_in_seconds, int song_count, String title) {
         this.artist = artist;
-        this.songCount = songCount;
-        this.length = length;
-        this.imageUrl = imageUrl;
+        this.image_url = image_url;
+        this.length_in_seconds = length_in_seconds;
+        this.song_count = song_count;
+        this.title = title;
     }
-
-    // Getters and setters
 
     public Long getId() {
         return id;
@@ -38,14 +37,6 @@ public class Album {
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getArtist() {
@@ -56,27 +47,35 @@ public class Album {
         this.artist = artist;
     }
 
-    public int getSongCount() {
-        return songCount;
+    public String getImage_url() {
+        return image_url;
     }
 
-    public void setSongCount(int songCount) {
-        this.songCount = songCount;
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
     }
 
-    public int getLength() {
-        return length;
+    public int getLength_in_seconds() {
+        return length_in_seconds;
     }
 
-    public void setLength(int length) {
-        this.length = length;
+    public void setLength_in_seconds(int length_in_seconds) {
+        this.length_in_seconds = length_in_seconds;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public int getSong_count() {
+        return song_count;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setSong_count(int song_count) {
+        this.song_count = song_count;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
