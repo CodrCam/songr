b7f96bd3365c9cf89a951fc39cde81caaa44862c
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2023-05-02 22:26:47 -0700

    update depends and ignore

--- src/main/java/com/chamcocat/songr/model/Album.java
+++ src/main/java/com/chamcocat/songr/model/Album.java
@@ -1,11 +1,26 @@
-package com.chamcocat.songr;
+package com.chamcocat.songr.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "albums")
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String artist;
     private int songCount;
     private int length;
     private String imageUrl;
+
+    public Album() {
+    }
 
     public Album(String title, String artist, int songCount, int length, String imageUrl) {
         this.title = title;
@@ -15,7 +30,15 @@ public class Album {
         this.imageUrl = imageUrl;
     }
 
-    // Getters and setters for each field
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
@@ -57,4 +80,3 @@ public class Album {
         this.imageUrl = imageUrl;
     }
 }
-
